5af8f60f63141c0d6870bec2e6569273d59f977e
Autolab <Autolab@DESKTOP-7N3BV5I>
2019-08-13 13:24:02 -0700

    pushing to GIT

---
+++ src/s1/PushToGit.java
@@ -0,0 +1,13 @@
+package s1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PushToGit {
+
+	@Test
+	public void sum()
+	{
+		Reporter.log("10+20=30",true);
+	}
+}
